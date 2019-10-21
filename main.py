91fc79350e2a4adaca8f62c485ba0086c8d0ff3a
MrPipin <suchkovkirill22@outlook.com>
2019-10-21 18:58:05 +0300

    My first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(15, 30)
+
+arr = list(range(5))
+
+for num in arr:
+    num = random.randint(15, 30)
+    print(num)
+    if num == 22:
+        break
+else:
+    print("good")
+
+
+
+
