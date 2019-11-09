80344c576fb9cde9d09a9ebe956d2f7055078b1d
MrPipin <suchkovkirill22@outlook.com>
2019-11-09 12:16:03 +0300

    Add BubbleSort

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import random
-
-number = random.randint(15, 30)
-
-arr = list(range(5))
-
-for num in arr:
-    num = random.randint(15, 30)
-    print(num)
-    if num == 22:
-        break
-else:
-    print("good")
-
-print("Hello")
+from random import randint
+ 
+N = 10
+a = []
+for i in range(N):
+    a.append(randint(1, 99))
+print(a)
+ 
+ 
+for i in range(N-1):
+    for j in range(N-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+ 
+print(a)
